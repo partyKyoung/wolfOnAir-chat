0de96c696c7f5ec6627dad48be3c82a231e65ac5
partykyoung <kyoungah@kyoungah.com>
2021-02-08 22:38:58 +0900

    feat: 채팅 메시지 전송 작업

--- src/socket.ts
+++ src/socket.ts
@@ -16,20 +16,29 @@ function socketIo(server: HttpServer, app: Koa) {
   const room = io.of('/room');
   const lobby = io.of('/lobby');
 
-  lobby.on('connect', () => {});
+  lobby.on('connection', () => {});
 
-  room.on('connect', () => {});
+  room.on('connection', () => {});
 
   chat.on('connect', (socket) => {
     const { handshake: { query } } = socket;
 
     socket.join(query.room);
 
-    chat.to(query.room).emit('join', {
-      type: 'system',
-      userName: 'system',
-      message: `${query.userName}님이 입장하셨습니다.`
-    })
+    // chat.to(query.room).emit('join', {
+    //   type: 'system',
+    //   userName: 'system',
+    //   message: `${query.userName}님이 입장하셨습니다.`
+    // });
+
+    // 메시지 전송
+    socket.on('sendMessage' , (data: any) => {
+      chat.to(data.room).emit('getMessage', {
+        type: 'user',
+        userName: data.userName,
+        message: data.message
+      })
+    });
   });
 };
 
